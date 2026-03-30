2ccd4856e43eeb69559b48cac08b9b87dab6d1a1
Sanjay-t01 <sanjaypvt06@gmail.com>
2026-03-30 09:53:40 +0530

    feature/UC5

---
+++ src/OopsBannerApp.java
@@ -0,0 +1,17 @@
+class OopsBannerApp{
+    public static void main(String [] args){
+       String[] lines = {
+    String.join(" ", " *** ", " *** ", " **** ", " **** "),
+    String.join(" ", "** **", "** **", "**  **", "**   "),
+    String.join(" ", "** **", "** **", "**** ", " *** "),
+    String.join(" ", "** **", "** **", "**   ", "   **"),
+    String.join(" ", "** **", "** **", "**   ", "**  **"),
+    String.join(" ", " *** ", " *** ", "**   ", " **** ")
+};
+
+// Then use a for-each loop to print each line
+for (String line : lines) {
+    System.out.println(line);
+}
+    }
+}
