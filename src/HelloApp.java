d9420b2c17298e425bde2902b4c8f3fbfa7bcef3
Sanjay-t01 <sanjaypvt06@gmail.com>
2026-03-30 09:03:10 +0530

    feature/uc3

--- src/HelloApp.java
+++ src/HelloApp.java
@@ -1,13 +1,10 @@
-
-public class HelloApp {
-    public static void main(String[] args){
-        System.out.println("   *******     *******   *******   ********");
-        System.out.println("  **     **   **     **  **    ** **       ");
-        System.out.println(" **       ** **       ** **    ** **       ");
-        System.out.println(" **       ** **       ** *******  *********");
-        System.out.println(" **       ** **       ** **              **");
-        System.out.println("  **     **   **     **  **              **");
-        System.out.println("   *******     *******   **       *********");
+class OopsBannerApp{
+    public static void main(String [] args){
+      System.out.println(String.join(" ", " *** ", " *** ", " **** ", " **** "));
+System.out.println(String.join(" ", "** **", "** **", "**  **", "**   "));
+System.out.println(String.join(" ", "** **", "** **", "**** ", " *** "));
+System.out.println(String.join(" ", "** **", "** **", "**   ", "   **"));
+System.out.println(String.join(" ", "** **", "** **", "**   ", "**  **"));
+System.out.println(String.join(" ", " *** ", " *** ", "**   ", " **** "));
     }
-    
 }
